374ab5ee4dc184a7119c52126b9e58bf23d1e0cf
Brett Evrist <brettevrist10@gmail.com>
2018-11-20 02:11:46 -0800

    Added Speech related ScriptableObjects
    created Speech input and output scriptable objects, with accompanying editor scripts.
    still need to implement auto generation of missing audio clips

---
+++ Assets/Scripts/Speech/GenerateOutputClips.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace speech
+{
+    public class GenerateOutputClips : MonoBehaviour
+    {
+
+        //TODO properly generate and attach audio for all output objects
+        //will need to extend cloud manager for this purpose
+
+    }
+}
---
+++ Assets/Scripts/Speech/SpeechOutput.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace speech
+{
+    [CreateAssetMenu(menuName = "Speech/Speech Output Object")]
+    public class SpeechOutput : ScriptableObject
+    {
+
+        public string[] OutputPhrases = new string[1];
+        public AudioClip[] OutputClips = new AudioClip[1]; //<- multi dimensional array to handle multiple "patient" voices?
+
+    }
+}

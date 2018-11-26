cc3e28eab4668890bdd29b20ccc26640650491c3
Brett Evrist <brettevrist10@gmail.com>
2018-11-26 09:16:00 -0800

    Removed linked object from SpeechInput.cs
    removed linked objects to better follow single responsibility principle

--- Assets/Scripts/Speech/SpeechInput.cs
+++ Assets/Scripts/Speech/SpeechInput.cs
@@ -6,8 +6,6 @@ namespace Speech
     [CreateAssetMenu(menuName = "Speech/Speech Input Object")]
     public class SpeechInput : ScriptableObject
     {
-        /// linked SpeechOutput object
-        public SpeechOutput outputObject;
         /// list of similar phrases for input
         public string[] inputPhrases = new string[1];
     }

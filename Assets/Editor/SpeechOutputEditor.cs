4dbf595d001f5fce3d5f4dc8a4764e9a16ffa249
Unknown <005705423@coyote.csusb.edu>
2018-11-27 10:47:59 -0800

    Fixed Build Issue From Editor Scripts
    Editor scripts need to be in a folder named "Editor" in order to build without including them in the package. The namespace UnityEditor cannot be accessed on anything used in the final build.

---
+++ Assets/Editor/SpeechOutputEditor.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace speech
+{
+    [CustomEditor(typeof(SpeechOutput), true)]
+    public class SpeechOutputEditor : Editor {
+
+        //TODO create button to generate and play speech sample in editor properly
+        //will need to extend cloud manager for this purpose
+
+        //TODO reduce number of ((casts)) in this file
+
+        int AudioClipSelection = 0;
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            if (GUILayout.Button("PreviewClip"))
+            {
+                Debug.Log("Preview Clip: " + (string)((SpeechOutput)target).OutputPhrases[AudioClipSelection]);
+                Debug.LogWarning("preview not implemented");
+                //TODO
+            }
+
+            string[] OutputPhraseArray = ((SpeechOutput)target).OutputPhrases;
+
+            AudioClipSelection = EditorGUILayout.IntField("selection:", AudioClipSelection);
+            if (AudioClipSelection > OutputPhraseArray.Length)
+            {
+                string tmp = "Selection Out of Bounds";
+                EditorGUILayout.TextField("ERROR:", tmp);
+            }
+
+            if (OutputPhraseArray.Length > ((SpeechOutput)target).OutputClips.Length)
+            {
+                AudioClip[] temp = new AudioClip[OutputPhraseArray.Length];
+                ((SpeechOutput)target).OutputClips.CopyTo(temp, 0);
+                ((SpeechOutput)target).OutputClips = temp;
+            }
+            else if (OutputPhraseArray.Length < ((SpeechOutput)target).OutputClips.Length)
+            {
+                SerializedObject serializedObject1 = new UnityEditor.SerializedObject(((SpeechOutput)target));
+                Debug.LogError(serializedObject1.FindProperty("m_Name").stringValue +
+                    ": OutputPhrasesArray shorter than OutputClipArray. Reduce the OutputClip Array size.");
+            }
+        }
+    }
+}

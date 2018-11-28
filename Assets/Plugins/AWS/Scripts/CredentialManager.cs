27cf9c67ffa1985afb8a42ce2a0b162be8b263ce
Brett Evrist <brettevrist10@gmail.com>
2018-11-27 22:13:00 -0800

    Set up external AWS credentials file loader

--- Assets/Plugins/AWS/Scripts/CredentialManager.cs
+++ Assets/Plugins/AWS/Scripts/CredentialManager.cs
@@ -1,28 +1,26 @@
-﻿
+﻿using SharpConfig; // https://github.com/cemdervis/SharpConfig
+using System.IO;
+using Amazon.Runtime;
+
 namespace AWS
 {
     public static class CredentialManager
     {
 
-        private static string AWSaccessKey = "";    //TODO allow credential setup from external file
-        private static string AWSsecretKey = "";
-        private static string AWSsecretToken = "";
+        private static string AWSaccessKey;
+        private static string AWSsecretKey;
+        private static string AWSsecretToken;
+
         private static Amazon.RegionEndpoint AWSregionEndpoint = Amazon.RegionEndpoint.USEast1; //TODO allow change of region
 
-        private static Credential AWSCredintial;
+        private static AWSCredentials AWSCredintial;
 
-        static CredentialManager() { 
-            if (AWSsecretToken != ""){  //if AWSsecretToken is used
-            AWSCredintial = new Credential(AWSaccessKey, AWSsecretKey, AWSsecretToken);
-            }
-            //else{   //TODO if AWSsecretToken is not used
-            //AWSCredintial = new Amazon.Runtime.AWSCredential(AWSaccessKey, AWSsecretKey);
-            //    Amazon.Runtime.BasicAWSCredentials(AWSaccessKey, AWSsecretKey);
-            //}
+        static CredentialManager() {
+            UpdateCredentials();
         }
 
         /// returns user AWSCredential object
-        public static Credential getCredential()
+        public static AWSCredentials getCredential()
         {
             return AWSCredintial;
         }
@@ -32,5 +30,50 @@ namespace AWS
         {
             return AWSregionEndpoint;
         }
+
+        private static void UpdateCredentials()
+        {
+            LoadCredentialsFromConfigFile();
+
+            if (AWSaccessKey == "")
+            {
+                UnityEngine.Debug.LogError("AWS credentials file is empty!");
+            }
+
+            if (AWSsecretToken != "")
+            {  //if AWSsecretToken is used
+                AWSCredintial = new SessionAWSCredentials(AWSaccessKey, AWSsecretKey, AWSsecretToken);
+            }
+            else
+            {   //if AWSsecretToken is not used
+                AWSCredintial = new BasicAWSCredentials(AWSaccessKey, AWSsecretKey);
+            }
+        }
+
+        //FIXME private
+        public static void LoadCredentialsFromConfigFile()
+        {
+
+            string settingsFileName = "AWS_CONFIG.cfg";
+            Configuration AWScfg = new Configuration();
+
+            //if file not found, create default file
+            if (!File.Exists(settingsFileName))
+            {
+                AWScfg["default"]["aws_access_key_id"].StringValue = "";
+                AWScfg["default"]["aws_secret_access_key"].StringValue = "";
+                AWScfg["default"]["aws_session_token"].StringValue = "";
+
+                AWScfg.SaveToFile(settingsFileName);
+            }
+
+            AWScfg = Configuration.LoadFromFile(settingsFileName);
+
+            //Load data from config file
+            AWSaccessKey = AWScfg["default"]["aws_access_key_id"].StringValue;
+            AWSsecretKey = AWScfg["default"]["aws_secret_access_key"].StringValue;
+            AWSsecretToken = AWScfg["default"]["aws_session_token"].StringValue;
+        }
+
     }
 }

c3d4c802e9265ab39e3574be448b122a1e4e34ec
Unknown <gman10501@gmail.com>
2018-11-30 16:55:40 -0800

    Behavior and Dynamic DialogueTree in progress

--- Assets/Scripts/PatientAI.cs
+++ Assets/Scripts/PatientAI.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NodeCanvas.Framework;
 
 public class PatientAI : MonoBehaviour
 {
-    
+    public GlobalBlackboard globalBlackboard;
+    public string dialogueString;
+    public AudioClip dialogueAudioClip;
+
 	// Use this for initialization
 	void Start () 
 	{
-		
+        
 	}
 	
 	// Update is called once per frame
@@ -16,4 +20,10 @@ public class PatientAI : MonoBehaviour
 	{
 		
 	}
+
+    public void SetDialogue()
+    {
+        globalBlackboard.SetValue("patientDialogue", dialogueString);
+        globalBlackboard.SetValue("dialogueAudioClip", dialogueAudioClip);
+    }
 }

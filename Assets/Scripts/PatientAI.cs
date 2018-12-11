4693279ebdd230956565e6368a882fcc41f1834d
Unknown <gman10501@gmail.com>
2018-12-11 14:59:55 -0800

    Finished initial setup of FSM, Behavior trees, and dialogue trees

--- Assets/Scripts/PatientAI.cs
+++ Assets/Scripts/PatientAI.cs
@@ -5,26 +5,51 @@ using NodeCanvas.Framework;
 
 public class PatientAI : MonoBehaviour
 {
-    public GlobalBlackboard globalBlackboard;
-    public string dialogueString;
-    public AudioClip dialogueAudioClip;
+    [Header("Dialogue for AI: ")]
+    public string dialogueString;                   // String data for the dialogue
+    public AudioClip dialogueAudioClip;             // AudioClip data for the dialogue
 
-	// Use this for initialization
-	void Start () 
-	{
-        
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		
-	}
+    private GlobalBlackboard globalBlackboard;      // Global Blackboard is used to access the variables that all Node Canvas trees can used
 
+    /// <summary>
+    /// Initializes variables upon scene awake
+    /// </summary>
+    private void Awake()
+    {
+        globalBlackboard = FindObjectOfType<GlobalBlackboard>();    
+    }
+
+    /// <summary>
+    /// Used to find a greeting from a dialogue collection
+    /// </summary>
+    public void FindGreetingDialogue()
+    {
+
+    }
+
+    /// <summary>
+    /// Used to find a question to ask from a dialogue collection
+    /// </summary>
+    public void FindQuestionDialogue()
+    {
+
+    }
+
+    /// <summary>
+    /// Sets dialogue after finding the dialogue of type
+    /// </summary>
     public void SetDialogue()
     {
         globalBlackboard.SetValue("patientDialogue", dialogueString);
         globalBlackboard.SetValue("dialogueAudioClip", dialogueAudioClip);
+    }
+
+    /// <summary>
+    /// Verifying that a dialogue is finished
+    /// (Might be a temporary function until we check for flags)
+    /// </summary>
+    public void AIDialogueFinished()
+    {
         globalBlackboard.SetValue("isAIDialogueFinished", true);
     }
 }

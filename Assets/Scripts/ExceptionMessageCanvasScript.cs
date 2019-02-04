8ce5f1bbf23faa3a9624f099b774c219bbffe865
Ajay Kotian <005411285@coyote.csusb.edu>
2019-02-04 11:06:17 -0800

    Added functionality to display exception messages on screen

---
+++ Assets/Scripts/ExceptionMessageCanvasScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ExceptionMessageCanvasScript : MonoBehaviour {
+
+    private Canvas exceptionMessageCanvasObject;
+
+    private Text exceptionMessageCanvasTextObject;
+
+    void Awake()
+    {
+        Canvas[] canvasObjects = GetComponents<Canvas>();
+        this.exceptionMessageCanvasObject = canvasObjects[0];
+        Text[] canvasTextObjects = GetComponentsInChildren<Text>();
+        this.exceptionMessageCanvasTextObject = canvasTextObjects[0];
+        this.exceptionMessageCanvasObject.scaleFactor = 0;
+    }
+
+    void OnEnable()
+    {
+        Application.logMessageReceived += DisplayMessageOnCanvas;
+    }
+
+    private void DisplayMessageOnCanvas(string logString, string stackTrace, LogType type)
+    {
+        if (type == LogType.Exception)
+        {
+            this.exceptionMessageCanvasObject.scaleFactor = 2;
+            this.exceptionMessageCanvasTextObject.text = logString + "\n Check the console for stack trace.";
+            this.exceptionMessageCanvasTextObject.color = Color.red;
+        }
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= DisplayMessageOnCanvas;
+    }
+}
